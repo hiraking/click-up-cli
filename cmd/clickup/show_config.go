a959e76662c9ada822472c47e21dd846da7779b4
hirakin <hirakitaito@gmail.com>
2026-05-06 23:02:37 +0900

    feat: add show-config command

---
+++ cmd/clickup/show_config.go
@@ -0,0 +1,30 @@
+// cmd/clickup/show_config.go
+package main
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func newShowConfigCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "show-config",
+		Short: "Show current configuration as JSON",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cfg, err := loadConfig()
+			if err != nil {
+				return err
+			}
+			out := struct {
+				APIKey string            `json:"apiKey"`
+				TeamID string            `json:"teamId"`
+				Lists  map[string]string `json:"lists"`
+			}{
+				APIKey: maskAPIKey(cfg.APIKey),
+				TeamID: cfg.TeamID,
+				Lists:  cfg.Lists,
+			}
+			return printJSON(out)
+		},
+	}
+	return cmd
+}
